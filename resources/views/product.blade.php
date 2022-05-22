6f1f7051c209ef068c06795c11038ecdb98d9a1c
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-22 18:02:48 +0700

    auth link to product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -94,6 +94,19 @@
                                 <li class="nav-item">
                                     <a data-scroll-nav="0" href="#contact">Contact</a>
                                 </li>
+                                @if (Route::has('login'))
+                                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                        @auth
+                                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                        @else
+                                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                                            @if (Route::has('register'))
+                                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                            @endif
+                                        @endauth
+                                    </div>
+                                @endif
                             </ul> <!-- navbar nav -->
                         </div>
                     </nav> <!-- navbar -->
