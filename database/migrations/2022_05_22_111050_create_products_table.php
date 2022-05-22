9a68d9dbcd34b61faacb68b935d9b66254aa0a1e
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-22 18:16:42 +0700

    add product table attribute

--- database/migrations/2022_05_22_111050_create_products_table.php
+++ database/migrations/2022_05_22_111050_create_products_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('image_name');
+            $table->string('price');
+            $table->string('sale_price');
             $table->timestamps();
         });
     }
