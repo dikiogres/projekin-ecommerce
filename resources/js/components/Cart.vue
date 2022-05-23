401fd264fb90da39765c6b4dd93cebee2c2d6536
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-23 19:20:23 +0700

    create cart component

---
+++ resources/js/components/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <li class="nav-item">
+            <a href="/home" class="btn btn-warning btn sm">Cart</a>
+        </li>       
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
