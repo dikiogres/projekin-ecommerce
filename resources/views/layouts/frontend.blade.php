4b4a8f154df48a72e5810ae15a31f9fe0134629b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-23 11:07:35 +0700

    dynamic layout #2

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -10,8 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <!--====== Title ======-->
-    <title>Furnish - Furniture and Decor Website Template</title>
-    
+    <title>Projekin</title>
+    <script src="{{ asset('css/app.css') }}" defer></script>
     <!--====== Favicon Icon ======-->
     <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
 
@@ -44,90 +44,96 @@
 
 
 <body>
-
     
-    <!--====== PRELOADER PART START ======-->
-    
-    <div class="preloader">
-        <div class="spin">
-            <div class="cube1"></div>
-            <div class="cube2"></div>
-        </div>
-    </div>
-    
-    <!--====== PRELOADER PART START ======-->
-    
-    <!--====== HEADER PART START ======-->
-    
-    <header class="header-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <nav class="navbar navbar-expand-lg">
-                        <a class="navbar-brand" href="index.html">
-                            <img src="assets/images/logo.png" alt="Logo">
-                        </a> <!-- Logo -->
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="bar-icon"></span>
-                            <span class="bar-icon"></span>
-                            <span class="bar-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul id="nav" class="navbar-nav ml-auto">
-                                <li class="nav-item active">
-                                    <a data-scroll-nav="0" href="#home">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#product">Products</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#service">Services</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#showcase">Portfolio</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#team">Team</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#blog">Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a data-scroll-nav="0" href="#contact">Contact</a>
-                                </li>
-                                @if (Route::has('login'))
-                                        @auth
-                                            <li class="nav-item">
-                                                <a href="{{ url('/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                                            </li>
-                                        @else
-                                            <li class="nav-item">
-                                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-                                            </li>
-                                            @if (Route::has('register'))
-                                            <li class="nav-item">
-                                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                                            </li>
-                                            @endif
-                                        @endauth
-                                @endif
-                            </ul> <!-- navbar nav -->
-                        </div>
-                    </nav> <!-- navbar -->
-                </div>
-            </div> <!-- row -->
-        </div> <!-- container -->
-    </header>
     
     <!--====== HEADER PART ENDS ======-->
+    <div id="app">
+
+        <!--====== PRELOADER PART START ======-->
+        
+        <div class="preloader">
+            <div class="spin">
+                <div class="cube1"></div>
+                <div class="cube2"></div>
+            </div>
+        </div>
+        
+        <!--====== PRELOADER PART START ======-->
+        
+        <!--====== HEADER PART START ======-->
+        
+        <header class="header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand" href="index.html">
+                                <img src="assets/images/logo.png" alt="Logo">
+                            </a> <!-- Logo -->
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="bar-icon"></span>
+                                <span class="bar-icon"></span>
+                                <span class="bar-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul id="nav" class="navbar-nav ml-auto">
+                                    <li class="nav-item active">
+                                        <a data-scroll-nav="0" href="#home">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#product">Products</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#service">Services</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#showcase">Portfolio</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#team">Team</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#blog">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a data-scroll-nav="0" href="#contact">Contact</a>
+                                    </li>
+                                    @if (Route::has('login'))
+                                            @auth
+                                                <li class="nav-item">
+                                                    <a href="{{ url('/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                                </li>
+                                            @else
+                                                <li class="nav-item">
+                                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                                                </li>
+                                                @if (Route::has('register'))
+                                                <li class="nav-item">
+                                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                                </li>
+                                                @endif
+                                            @endauth
+                                    @endif
+                                </ul> <!-- navbar nav -->
+                            </div>
+                        </nav> <!-- navbar -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </header>
+
+        @yield('content')
+
+    </div>
 
     @yield('content')
 
-
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <!--====== jquery js ======-->
        <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
 
     <!--====== Bootstrap js ======-->
     <script src="assets/js/bootstrap.min.js"></script>
