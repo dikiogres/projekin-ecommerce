1e733364a6bf4263566f2fbdf6a3be4c824aaa7d
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-23 11:31:15 +0700

    adding to cart processing using axios

--- resources/js/components/AddToCart.vue
+++ resources/js/components/AddToCart.vue
@@ -15,11 +15,22 @@
         },
         props:['productId', 'userId'],
         method:{
-            addProductToCart(){
+            async addProductToCart(){
+
+                //checking if user logged in.
+
                 if(this.userId == 0){
                     this.$toastr.e('You Need to Login,  to add this product in Cart');
                     return;
                 }
+
+                //if user logged in then add item to cart.
+
+                let  response =  await axios.post('/cart', {
+                    'product_id': this.productId
+                })
+
+                console.log(response.data);
             }
         },
         mounted() {
