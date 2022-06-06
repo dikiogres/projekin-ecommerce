baa87559e719a13e7e59f85c428cf9c57f139c3d
BlazeSyn <frederick.wijayadisusilo@gmail.com>
2022-06-06 17:43:41 +0700

    Add checkout

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -17,9 +17,40 @@
     </h3>
     <div class="container" style="display: flex">
         <div class="card">
-            <header class="sec-head">
-
-            </header>
+            <div class="full">
+                <header class="sec-head">
+                    <h4 class="payment-method">
+                        Payment Method
+                    </h4>
+                </header>
+                <div class="card">
+                    <div class="payment-list">
+                        <div class="cred">
+                            <div class="pay-cred">
+                                <div class="pay-header">
+                                    <span class="cc-title">Credit & Debit Cards</span>
+                                    <ul style="display: flex">
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/1.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/2.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/3.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/4.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/5.png" alt=""></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="payment-method-container">
+                            <div>
+                                <form class="payment-form">
+                                    <div class="card-number-form">
+                                        
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
