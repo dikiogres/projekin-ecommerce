970eaa7831234aefc282109bd7618bb60189fa1c
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-06-06 07:36:33 +0700

    [ADD]CRUD Brand & Table brand

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -14,98 +14,98 @@ class BrandController extends Controller
         return view('backend.brand.brand-view',compact('brands'));
     }
 
-    // public function brandStore(Request $request){
-    //     $request->validate([
-    //         'brand_name_en' => 'required',
-    //         'brand_name_ind' => 'required',
-    //         'brand_image' => 'required',
-    //     ],
-    //     [
-    //         'brand_name_en.required' => 'Input Brand English Name',
-    //         'brand_name_ind.required' => 'Input Brand Indonesia Name',
-    //     ]);
-    //     $image = $request->file('brand_image');
-    //     $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-    //     Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
-    //     $save_url = 'upload/brand/'.$name_gen;
+    public function brandStore(Request $request){
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_ind' => 'required',
+            'brand_image' => 'required',
+        ],
+        [
+            'brand_name_en.required' => 'Input Brand English Name',
+            'brand_name_ind.required' => 'Input Brand Indonesia Name',
+        ]);
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url = 'upload/brand/'.$name_gen;
         
-    //     Brand::insert([
-	// 	'brand_name_en' => $request->brand_name_en,
-	// 	'brand_name_ind' => $request->brand_name_ind,
-	// 	'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
-	// 	'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
-	// 	'brand_image' => $save_url,
-    //     ]);
+        Brand::insert([
+		'brand_name_en' => $request->brand_name_en,
+		'brand_name_ind' => $request->brand_name_ind,
+		'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
+		'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
+		'brand_image' => $save_url,
+        ]);
         
-    //     $notification = array(
-	// 		'message' => 'Brand Inserted Successfully',
-	// 		'alert-type' => 'success'
-	// 	);
+        $notification = array(
+			'message' => 'Brand Inserted Successfully',
+			'alert-type' => 'success'
+		);
 
-	// 	return redirect()->back()->with($notification);
-    // }
+		return redirect()->back()->with($notification);
+    }
 
-    // public function brandEdit($id){
-    //     $brand = Brand::findOrFail($id);
-    //     return view('backend.brand.brand-edit',compact('brand'));
-    // }
+    public function brandEdit($id){
+        $brand = Brand::findOrFail($id);
+        return view('backend.brand.brand-edit',compact('brand'));
+    }
 
-    // public function brandUpdate(Request $request){
-    //     $brand_id = $request->id;
-    //     $old_img = $request->old_image;
+    public function brandUpdate(Request $request){
+        $brand_id = $request->id;
+        $old_img = $request->old_image;
 
-    //     if ($request->file('brand_image')) {
+        if ($request->file('brand_image')) {
 
-    //         unlink($old_img);
-    //         $image = $request->file('brand_image');
-    //         $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-    //         Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
-    //         $save_url = 'upload/brand/'.$name_gen;
+            unlink($old_img);
+            $image = $request->file('brand_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+            $save_url = 'upload/brand/'.$name_gen;
 
-    //         Brand::findOrFail($brand_id)->update([
-    //             'brand_name_en' => $request->brand_name_en,
-    //             'brand_name_ind' => $request->brand_name_ind,
-    //             'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
-    //             'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
-    //             'brand_image' => $save_url,
-    //         ]);         
+            Brand::findOrFail($brand_id)->update([
+                'brand_name_en' => $request->brand_name_en,
+                'brand_name_ind' => $request->brand_name_ind,
+                'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
+                'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
+                'brand_image' => $save_url,
+            ]);         
 
-    //         $notification = array(
-    //             'message' => 'Brand Updated Successfully',
-    //             'alert-type' => 'info'
-    //         );          
+            $notification = array(
+                'message' => 'Brand Updated Successfully',
+                'alert-type' => 'info'
+            );          
 
-    //         return redirect()->route('all.brand')->with($notification);
-    //     }
-    //     else{
-    //         Brand::findOrFail($brand_id)->update([
-    //             'brand_name_en' => $request->brand_name_en,
-    //             'brand_name_ind' => $request->brand_name_ind,
-    //             'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
-    //             'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
-    //         ]);
+            return redirect()->route('all.brand')->with($notification);
+        }
+        else{
+            Brand::findOrFail($brand_id)->update([
+                'brand_name_en' => $request->brand_name_en,
+                'brand_name_ind' => $request->brand_name_ind,
+                'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
+                'brand_slug_ind' => str_replace(' ', '-',$request->brand_name_ind),
+            ]);
 
-    //         $notification = array(
-    //             'message' => 'Brand Updated Successfully',
-    //             'alert-type' => 'info'
-    //         );
+            $notification = array(
+                'message' => 'Brand Updated Successfully',
+                'alert-type' => 'info'
+            );
             
-    //         return redirect()->route('all.brand')->with($notification);
-    //     }
-    // }
+            return redirect()->route('all.brand')->with($notification);
+        }
+    }
 
-    // public function brandDelete($id){
-    //     $brand = Brand::findOrFail($id);
-    //     $img = $brand->brand_image;
-    //     unlink($img);
+    public function brandDelete($id){
+        $brand = Brand::findOrFail($id);
+        $img = $brand->brand_image;
+        unlink($img);
         
-    //     Brand::findOrFail($id)->delete();
+        Brand::findOrFail($id)->delete();
         
-    //     $notification = array(
-	// 		'message' => 'Brand Deleted Successfully',
-	// 		'alert-type' => 'info'
-	// 	);
+        $notification = array(
+			'message' => 'Brand Deleted Successfully',
+			'alert-type' => 'info'
+		);
 
-	// 	return redirect()->back()->with($notification);
-    // }
+		return redirect()->back()->with($notification);
+    }
 }
