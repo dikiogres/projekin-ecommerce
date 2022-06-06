84466411370c2ef34d50a1960565e3e1d6ac299f
BlazeSyn <frederick.wijayadisusilo@gmail.com>
2022-06-06 19:59:13 +0700

    Add checkout

---
+++ resources/views/frontend/checkout.blade.php
@@ -0,0 +1,64 @@
+@extends('frontend.main-master')
+@section('content');
+
+<div>
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-inner">
+                <ul class="list-inline list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li class="active">Checkout</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <h3 class="container">
+        Checkout Order
+    </h3>
+    <div class="container" style="display: flex">
+        <div class="card">
+            <div class="full">
+                <header class="sec-head">
+                    <h4 class="payment-method">
+                        Payment Method
+                    </h4>
+                </header>
+                <div class="card">
+                    <div class="payment-list">
+                        <div class="cred">
+                            <div class="pay-cred">
+                                <div class="pay-header">
+                                    <span class="cc-title">Credit & Debit Cards</span>
+                                    <ul style="display: flex">
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/1.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/2.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/3.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/4.png" alt=""></li>
+                                        <li><img src="http://127.0.0.1:8000/frontend/assets/images/payments/5.png" alt=""></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="payment-method-container">
+                            <div>
+                                <form class="payment-form">
+                                    <div class="card-number-form">
+                                        <label>
+                                            Card Number
+                                        </label>
+                                        <div class="card-number-input error">
+                                            <input type="text"/>
+                                            <span class="card-block"></span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
