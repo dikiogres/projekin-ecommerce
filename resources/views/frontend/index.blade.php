abcf4f0e58eb2d9e1814493b8f05d04a95ce603b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-06-09 03:03:14 +0700

    feat: dynamic info boxes

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -15,79 +15,11 @@ Home | Projekin
             <!-- ============================================== CONTENT ============================================== -->
             <div class="col-xs-12 col-sm-12 col-md-9 homebanner-holder">
                 <!-- ========================================== SECTION – HERO ========================================= -->
-
-                <div id="hero">
-                    <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
-                        @foreach($sliders as $slider)
-                        <div class="item" style="background-image: url({{ asset($slider->slider_img) }});">
-                            <div class="container-fluid">
-                                <div class="caption bg-color vertical-center text-left">
-
-                                    <div class="big-text fadeInDown-1">{{ $slider->title }}</div>
-                                    <div class="excerpt fadeInDown-2 hidden-xs"> <span>{{ $slider->description }}</span>
-                                    </div>
-                                    <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product"
-                                            class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
-                                    </div>
-                                </div>
-                                <!-- /.caption -->
-                            </div>
-                            <!-- /.container-fluid -->
-                        </div>
-                        <!-- /.item -->
-                        @endforeach
-                    </div>
-                    <!-- /.owl-carousel -->
-                </div>
-
+                @include('frontend.landing.hero')
                 <!-- ========================================= SECTION – HERO : END ========================================= -->
 
                 <!-- ============================================== INFO BOXES ============================================== -->
-                <div class="info-boxes wow fadeInUp">
-                    <div class="info-boxes-inner">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-4 col-lg-4">
-                                <div class="info-box">
-                                    <div class="row">
-                                        <div class="col-xs-12">
-                                            <h4 class="info-box-heading green">money back</h4>
-                                        </div>
-                                    </div>
-                                    <h6 class="text">30 Days Money Back Guarantee</h6>
-                                </div>
-                            </div>
-                            <!-- .col -->
-
-                            <div class="hidden-md col-sm-4 col-lg-4">
-                                <div class="info-box">
-                                    <div class="row">
-                                        <div class="col-xs-12">
-                                            <h4 class="info-box-heading green">free shipping</h4>
-                                        </div>
-                                    </div>
-                                    <h6 class="text">Shipping on orders over $99</h6>
-                                </div>
-                            </div>
-                            <!-- .col -->
-
-                            <div class="col-md-6 col-sm-4 col-lg-4">
-                                <div class="info-box">
-                                    <div class="row">
-                                        <div class="col-xs-12">
-                                            <h4 class="info-box-heading green">Special Sale</h4>
-                                        </div>
-                                    </div>
-                                    <h6 class="text">Extra $5 off on all items </h6>
-                                </div>
-                            </div>
-                            <!-- .col -->
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                    <!-- /.info-boxes-inner -->
-
-                </div>
-                <!-- /.info-boxes -->
+                @include('frontend.landing.info-boxes')
                 <!-- ============================================== INFO BOXES : END ============================================== -->
                 <!-- ============================================== SCROLL TABS ============================================== -->
                 <div id="product-tabs-slider" class="scroll-tabs outer-top-vs wow fadeInUp">
